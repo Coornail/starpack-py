4ae8f63da042ea52ad39bd06cf2f805d7c29ac27
Kornel Lugosi <coornail@gmail.com>
2020-12-06 13:51:56 +0100

    Speed up grayscale conversion

--- color.py
+++ color.py
@@ -1,14 +1,9 @@
 from numpy import empty
+from numpy import dot
 
 
 def to_grayscale(img):
-    bw = empty((img.shape[0], img.shape[1]))
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            bw[i, j] = img[i, j, 0]*.2126 + \
-                img[i, j, 1] * .7152 + img[i, j, 2] * .072
-
-    return bw
+    return dot(img[..., :3], [0.2989, 0.5870, 0.1140])
 
 
 def alignment_window(img, window_size):
