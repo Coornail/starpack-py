cc13075e19001ac63299a5c924ee9ace9c1690e4
Kornel Lugosi <coornail@gmail.com>
2020-12-11 14:47:58 +0100

    Use a brute force to get the approximate alignment

--- shift.py
+++ shift.py
@@ -1,14 +1,14 @@
 from scipy.ndimage import shift
-from scipy.optimize import basinhopping
+from scipy.optimize import basinhopping, brute
 import numexpr as ne
 
 
 def alignment_score(xshift, yshift, angle, ref, input):
     # out = rotate(input, angle, reshape=False)
-    out = shift(input, (xshift, yshift))
+    out = shift(input, (xshift, yshift), cval=-1)
     # diff = (abs(ref-out)).sum()
     diff = ne.evaluate('abs(ref-out)').sum()
-    # print(xshift, yshift, angle, diff)
+    print(xshift, yshift, angle, diff)
     return diff
 
 
@@ -25,6 +25,6 @@ def find_best_shift_minimize(ref, input):
         def __call__(self, guess):
             return alignment_score(guess[0], guess[1], 0, self.ref, self.input)
 
-    minimize_result = basinhopping(Minimizer(ref, input), [
-                                   0.0, 0.0], niter=10, interval=10, seed=1, minimizer_kwargs={'options': {'eps': 0.5}})
-    return minimize_result.x
+    minimize_result = brute(Minimizer(ref, input), [
+                            (-100, 100), (-100, 100)], Ns=50)
+    return basinhopping(Minimizer(ref, input), minimize_result, seed=1, stepsize=0.5, minimizer_kwargs={'options': {'eps': 10}}).x
