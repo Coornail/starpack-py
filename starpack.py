1073c267641ad9bd48ff380f0797aa841cf1b92e
Kornel Lugosi <coornail@gmail.com>
2020-12-14 10:25:18 +0100

    Collect bias images

--- starpack.py
+++ starpack.py
@@ -17,19 +17,20 @@ def collect_images(dir):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Stack deep-sky/planet images')
-    parser.add_argument('lights', help='light files')
-    parser.add_argument('--darks', nargs='?', help='dark files')
-    parser.add_argument('--bias', nargs='?', help='bias files')
+    parser.add_argument('lights', help='light directory')
+    parser.add_argument('--darks', nargs='?', help='dark directory')
+    parser.add_argument('--bias', nargs='?', help='bias directory')
 
     args = parser.parse_args()
 
     light_paths = collect_images(args.lights)
     dark_paths = []
+    bias_paths = []
     if (args.darks):
         dark_paths = collect_images(args.darks)
     bias_paths = []
     if (args.bias):
-        dark_paths = collect_images(args.bias)
+        bias_paths = collect_images(args.bias)
 
     img = starpack(light_paths, darkframe_paths=dark_paths)
     imwrite('out.tif', img, photometric='rgb')
