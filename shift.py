f841f5413371fd5524d826b713d7306228b41a7c
Kornel Lugosi <coornail@gmail.com>
2021-01-14 16:19:28 +0100

    Clean up shift.py

--- shift.py
+++ shift.py
@@ -1,30 +1,11 @@
-from os import terminal_size
-from scipy.ndimage import shift
-from scipy.optimize import basinhopping, brute
-import numexpr as ne
 import cv2
 import numpy as np
-from skimage.filters import threshold_otsu
-
-
-def alignment_score(xshift, yshift, angle, ref, input):
-    # out = rotate(input, angle, reshape=False)
-    out = shift(input, (xshift, yshift), cval=-1)
-    # diff = (abs(ref-out)).sum()
-    diff = ne.evaluate('abs(ref-out)').sum()
-    print(xshift, yshift, angle, diff)
-    return diff
 
 
 def find_best_shift_minimize(ref, inp):
     """
     Find the best alignment for an image based on a reference image.
     """
-
-    # warp_matrix = np.eye(3, 3, dtype=np.float32)
-    # criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10000, 0.1)
-    # (cc, warp_matrix) = cv2.findTransformECC(ref, inp, warp_matrix,
-    #                                          cv2.MOTION_HOMOGRAPHY, criteria, inputMask=None, gaussFiltSize=3)
 
     # Detect ORB features and compute descriptors.
     orb = cv2.ORB_create(2 << 14)
@@ -42,7 +23,6 @@ def find_best_shift_minimize(ref, inp):
     # Remove not so good matches
     numGoodMatches = int(len(matches) * 0.005)
     matches = matches[:numGoodMatches]
-    print(matches)
 
     # Draw top matches
     imMatches = cv2.drawMatches(
@@ -56,9 +36,7 @@ def find_best_shift_minimize(ref, inp):
     for i, match in enumerate(matches):
         points1[i, :] = keypoints1[match.queryIdx].pt
         points2[i, :] = keypoints2[match.trainIdx].pt
-    
+
     # Find homography
     h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
     return h
-
-    return warp_matrix
